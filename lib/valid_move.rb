850ab920c1e22fa5db7cf0f24113842d112f7642
Michael McGovern <mmcgovern.wework@gmail.com>
2019-03-12 18:09:48 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,8 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   if index.between(1,9)
-    if position_taken?(board, index)
-    
+    if !position_taken?(board, index)
+      true
   else
     
   end
